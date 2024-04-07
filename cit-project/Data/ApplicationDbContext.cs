3859fba5bff3f0a7fe308fcd4cc657798b5fff73
m4rkuss <markbu07@gmail.com>
2024-04-07 21:52:53 +0300

    add Student DbSet

--- cit-project/Data/ApplicationDbContext.cs
+++ cit-project/Data/ApplicationDbContext.cs
@@ -1,13 +1,28 @@
-﻿//using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-//using Microsoft.EntityFrameworkCore;
+﻿using cit_project.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
+
 namespace cit_project.Data
 {
-	public class ApplicationDbContext
+	public class ApplicationDbContext : IdentityDbContext
 	{
-		public ApplicationDbContext()
+		public DbSet<Student> Students {  get; set; }
+
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
 		{
 		}
-	}
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+			builder.Entity<Student>()
+				.HasOne(b => b.Group)
+				.WithMany(a => a.Students)
+				.HasForeignKey(b => b.GroupId);
+        }
+    }
 }
 
--- cit-project/Models/Group.cs
+++ cit-project/Models/Group.cs
@@ -4,5 +4,7 @@ namespace cit_project.Models
   {
     public int Id { get; set; }
     public string Name { get; set; }
+
+    public IEnumerable<Student> Students { get; set; }
   }
 }
